e33a318ac046302ecef9ef9783857ce766c7387c
Marcos Fernandes <markbombomb@gmail.com>
2022-07-18 15:12:52 -0300

    added reference

--- src/apps/management/models.py
+++ src/apps/management/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import Group
 from django.db import models
 
+# TODO: https://github.com/vmalaga/PasswordManager
 
-class Password(models.Model):
+class PassDB(models.Model):
+    name = models.Charfield(max_lenght=100)
     group = models.ForeigKey(Group)
